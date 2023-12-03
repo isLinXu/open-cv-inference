1b3556d38db414e4a085f546a15c95e4e915f9eb
isLinXu <2267379130@qq.com>
2023-12-04 02:04:34 +0800

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
-﻿#include <fstream>
-
+﻿// Created by gatilin on 2023/11/18.
+#include <iostream>
 #include <opencv2/opencv.hpp>
 #include <utility>
+
 #include "det/yolo/yolov5/yolov5.h"
+#include "det/yolo/yolov6/yolov6.h"
+
 #include "utils/type_helper.h"
+#include "utils/time_helper.h"
+#include "utils/logger_helper.h"
+#include "utils/file_helper.h"
+
+using namespace std;
 using namespace cv;
 
 // 颜色设置
@@ -11,56 +19,154 @@ const std::vector<cv::Scalar> colors = { cv::Scalar(255, 255, 0),
                                          cv::Scalar(0, 255, 0),
                                          cv::Scalar(0, 255, 255),
                                          cv::Scalar(255, 0, 0) };
-
-int yolov5_infer(bool is_cuda, const std::string& image_path, const std::string& classes_path, std::string weights_path, const std::string& output_path, bool is_show){
-    /**
-     * @param is_cuda: 是否使用GPU
-     * @param image_path: 输入图片路径
-     * @param classes_path: 类别文件路径
-     * @param weights_path: 权重文件路径
-     * @param output_path: 输出图片路径
-     * @param is_show: 是否显示图片
-     */
-    cv::Mat frame = cv::imread(image_path);
-    YoloV5Detector detector(is_cuda, classes_path,std::move(weights_path));
-    auto classNames = detector.class_list;
-    printf("class size: %zu\n", classNames.size());
-
-    std::vector<YoloV5Detector::Detection> output;
-    detector.detect(frame, output, classNames);
-
-    int detections = output.size();
-
-    for (int i = 0; i < detections; ++i)
-    {
-        auto detection = output[i];
-        auto box = detection.box;
-        auto classId = detection.class_id;
-        auto confidence = detection.confidence;
-        std::cout << "classId:" << classId << " label:" << classNames[classId].c_str() << " confidence:" << confidence << std::endl;
-        const auto color = colors[classId % colors.size()];
-        cv::rectangle(frame, box, color, 3);
-        cv::rectangle(frame, cv::Point(box.x, box.y - 20), cv::Point(box.x + box.width, box.y), color, cv::FILLED);
-        std::string label_name = classNames[classId] + ":"+ floatToString(confidence);
-        cv::putText(frame, label_name, cv::Point(box.x, box.y - 5), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
-    }
-    if(is_show!=false){
-        cv::imshow("output", frame);
-        cv::waitKey(0);
-        cv::destroyAllWindows();
-    }
-    cv::imwrite(output_path, frame);
+int opencv_test(){
+    cv::Mat image = cv::imread("../images/person.jpg");
+    cv::imshow("image", image);
+    cv::waitKey(0);
     return 0;
 }
 
+int logger_test(){
+    try {
+        Logger logger("../log/log.txt", DEBUG);  // 创建一个将日志消息写入"log.txt"文件的Logger对象，设置日志级别为DEBUG
+
+        // 示例：记录不同级别的日志消息
+        logger.error("An error occurred");
+        logger.warning("This is a warning");
+        logger.info("Program started");
+        logger.debug("Performing some calculations...");
+
+        int sum = 0;
+        for (int i = 0; i < 100; ++i) {
+            sum += i;
+        }
+
+        logger.debug("Calculations done. Result: " + std::to_string(sum));
+        logger.info("Program finished");
+
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+}
+
+
+int yolov5_infer(std::string& image_path,
+                 std::string& weights_path,
+                 std::string& cls_name_path,
+                 std::string output_path,
+                 bool is_show){
+    // Load image.
+    Mat frame;
+    frame = imread(image_path);
+    if (frame.empty()) {
+        cout << "Couldn't load image." << endl;
+        return -1;
+    }
+    Mat input_frame = frame.clone();
+
+    // Create YOLOv5 object.
+    YOLOv5 yolov5(weights_path, cls_name_path);
+
+    // Put efficiency information.
+    // The function getPerfProfile returns the overall time for inference(t) and the timings for each of the layers(in layersTimes)
+    int cycles = 2;
+    double total_time = 0;
+    double freq = getTickFrequency() / 1000;
+    Mat img;
+
+    Mat input = input_frame.clone();
+    img = yolov5.detect(input);
+    vector<double> layersTimes;
+    double t = yolov5.getPerfProfile(layersTimes);
+    total_time = total_time + t;
+
+    double avg_time = total_time / cycles;
+    string label = format("Average inference time : %.2f ms", avg_time / freq);
+    cout << label << endl;
+
+    putText(img, label, Point(20, 40), cv::FONT_HERSHEY_SIMPLEX, 0.7, Scalar(0, 0, 255));
+
+    if(is_show!=false){
+        const string& model_path = weights_path;
+        int start_index = model_path.rfind("/");
+        string model_name = model_path.substr(start_index + 1, model_path.length() - start_index - 6);
+        imshow("CppRun" + model_name, img);
+        waitKey(0);
+    }
+    cv::imwrite(output_path, img);
+
+    return 0;
+}
+
+int yolov6_infer(
+        std::string& image_path,
+        std::string& weights_path,
+        std::string& cls_name_path,
+        std::string output_path,
+        bool is_show){
+    // Put efficiency information.
+    YOLOv6 detector(weights_path, cls_name_path);
+
+    // Load image.
+    Mat frame;
+    frame = imread(image_path);
+    Mat input_frame = frame.clone();
+
+    // Perform detection.
+    Mat img = detector.detect(input_frame);
+
+    // Display the result.
+    if(is_show!=false){
+        const string& model_path = weights_path;
+        int start_index = model_path.rfind("/");
+        string model_name = model_path.substr(start_index + 1, model_path.length() - start_index - 6);
+        imshow("CppRun" + model_name, img);
+        waitKey(0);
+    }
+    cv::imwrite(output_path, img);
+    return 0;
+}
+
+
 int main(int argc, char **argv) {
     std::cout << "Runing Open-CV-Inference..." << std::endl;
-    bool is_cuda = argc > 1 && strcmp(argv[1], "cuda") == 0;
+    Time_Helper timer;
+    timer.start();  // 开始计时
+    Logger logger("../log/run_time.txt", DEBUG);  // 创建一个将日志消息写入"log.txt"文件的Logger对象，设置日志级别为DEBUG
+
+    bool is_cuda = false;
+//    bool is_show = false;
     bool is_show = true;
-    std::string image_path = "../images/zidane.jpg";
-    std::string classes_path = "../data/yolov5/classes.txt";
-    std::string weights_path = "../weights/yolov5/yolov5s.onnx";
-    std::string output_path = "../images/output/zidane.jpg";
-    yolov5_infer(is_cuda, image_path, classes_path, weights_path, output_path, is_show);
-    return 0;
+    // 设置路径
+    std::string image_path = "/Users/gatilin/CLionProjects/opencv-inference/images/person.jpg";
+//    std::string image_path = "/Users/gatilin/CLionProjects/opencv-inference/images/zidane.jpg";
+//    std::string classes_path = "/Users/gatilin/CLionProjects/opencv-inference/data/yolov5/classes.txt";
+//    std::string weights_path = "/Users/gatilin/CLionProjects/opencv-inference/weights/yolov5-6/yolov5n.onnx";
+    std::string weights_path = "/Users/gatilin/CLionProjects/opencv-inference/weights/yolov6/yolov6n.onnx";
+    std::string cls_name_path = "/Users/gatilin/CLionProjects/opencv-inference/det/yolo/yolov5/coco.names";
+//    std::string weights_path = "/Users/gatilin/CLionProjects/opencv-inference/weights/yolov5_6/yolov5s.onnx";
+//    std::string weights_path = "/Users/gatilin/CLionProjects/opencv-inference/weights/yolov5/yolov5s.onnx";
+    std::string output_path = "../images/output/output.jpg";
+    if (!fileExists(image_path) || !fileExists(cls_name_path) || !fileExists(weights_path)) {
+        std::cout << "image_path is not exist!" << std::endl;
+        return 1;
+    }
+
+    try {
+        // 执行
+        logger.info("Program started");
+        /**RUN MAIN IN HERE*/
+        // logger_test();
+        // opencv_test();
+        yolov5_infer(image_path, weights_path, cls_name_path, output_path, is_show);
+        yolov6_infer(image_path, weights_path, cls_name_path, output_path, is_show);
+        logger.info("Program finished");
+    } catch (const std::exception &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+    // 打印时间
+    timer.stop();  // 停止计时
+
 }
